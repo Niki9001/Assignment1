8fce7265b2cb5b25e97655a8aa2e9af2f314c815
Niki9001 <w0470221@nscc.ca>
2024-09-21 13:26:10 -0300

    initial conditions

--- Board.java
+++ Board.java
@@ -3,4 +3,26 @@ public class Board {
     public Board(){
         board = new Piece[8][8];
     }
+    public void movePiece(Piece piece, int[] position){
+        int x = position[0];
+        int y = position[1];
+        if (){
+
+        }
+        else {
+
+        }
+
+    }
+
+    public void removePiece(int[] position){
+        int x = position[0];
+        int y = position[1];
+        if (){
+
+        }
+        else {
+
+        }
+    }
 }

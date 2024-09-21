77ca1b7bd9e331d5ce77345d99655fb4646143c4
Niki9001 <w0470221@nscc.ca>
2024-09-21 12:31:24 -0300

    create structure of slow flex

--- SlowFlexiblePiece.java
+++ SlowFlexiblePiece.java
@@ -3,4 +3,28 @@ public class SlowFlexiblePiece extends Piece{
     public SlowFlexiblePiece (String name, String color, int[] position){
         super(name, color, position);
     }
+    public void move(String direction){
+        // same thing
+        direction = direction.toLowerCase();
+        int[] position = positionGetter();
+
+        switch (direction){
+            case "left":
+                break;
+            case "right":
+                break;
+            case "up":
+                break;
+            case "down":
+                break;
+        }
+    }
+    @Override
+    public String toString() {
+        int[] position = positionGetter();
+        return "Slow Flexible Piece: \n" +
+                "Name: " + nameGetter() + "\n" +
+                "Color: " + colorGetter() + "\n" +
+                "Position: [" + position[0] + "], [" + position[1] + "]";
+    }
 }

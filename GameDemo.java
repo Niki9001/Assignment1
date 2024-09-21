5ffa3017f862ae20882a0b4480b4c9dd6bd5b7cc
Niki9001 <w0470221@nscc.ca>
2024-09-21 14:07:03 -0300

    initial

---
+++ GameDemo.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class GameDemo {
+    public static void main(String[] args){
+        Board board = new Board();
+        Scanner scanner = new Scanner(System.in);
+    }
+}

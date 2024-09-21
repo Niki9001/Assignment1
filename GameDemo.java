1d345e75d89e090345239c6938cc5b0e9eacf2d6
Niki9001 <w0470221@nscc.ca>
2024-09-21 14:13:34 -0300

    initial conditions

--- GameDemo.java
+++ GameDemo.java
@@ -5,4 +5,22 @@ public class GameDemo {
         Board board = new Board();
         Scanner scanner = new Scanner(System.in);
     }
+    public static void dealWithInput(Board board, Scanner scanner){
+        System.out.println("Please input a command or input HELP for helping: ");
+        String input = scanner.nextLine();
+
+        if(input.contains("create")){
+
+        } else if (input.contains("move")) {
+
+        } else if (input.contains("help")) {
+
+        } else if (input.contains("exit")) {
+
+        }
+        else {
+            System.out.println("Please try it again.");
+        }
+        dealWithInput(board,scanner);
+    }
 }

17769674924a63a89dafd4e220082641f1b9f8bf
Niki9001 <w0470221@nscc.ca>
2024-09-21 11:28:19 -0300

    add parent class, and create position related keywords

--- SlowPiece.java
+++ SlowPiece.java
@@ -1,12 +1,18 @@
-public class SlowPiece {
+public class SlowPiece extends Piece{
     public void move(String direction){
         // to adapt both upper and lower case
         direction = direction.toLowerCase();
         switch (direction){
             case "left":
+                if (positionGetter()[0] > 0){
+
+                }
                 break;
 
             case "right":
+                if (positionGetter()[0]<=6){
+
+                }
                 break;
 
         }

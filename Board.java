ca10b6d545a570a67df6f47d6285fd336c20ae0d
Niki9001 <w0470221@nscc.ca>
2024-09-21 13:58:06 -0300

    debugging

--- Board.java
+++ Board.java
@@ -1,30 +1,52 @@
 public class Board {
     Piece[][] board;
-    public Board(){
+
+    // 构造函数，初始化棋盘，所有位置都设置为 EmptyPosition
+    public Board() {
         board = new Piece[8][8];
+        // 初始化每个位置为 EmptyPosition 占位符
+        for (int i = 0; i <= 7; i = i + 1) {
+            for (int j = 0; j <= 7; j = j + 1) {
+                board[i][j] = new EmptyPostion();  // 初始化为空位置
+            }
+        }
     }
-    public void movePiece(Piece piece, int[] position){
+
+    // 移动棋子到指定位置
+    public void movePiece(Piece piece, int[] position) {
         int x = position[0];
         int y = position[1];
-        if (x >= 0 && x <= 7 && y >= 0 && y <= 7){
-            board[x][y] = piece;
+        if (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
+            board[x][y] = piece;  // 将棋子放置到新的位置
             System.out.println("Assigned " + piece.nameGetter() + " to position [" + x + "][" + y + "]");
-        }
-        else {
-            System.out.println("out of range");
+        } else {
+            System.out.println("Position out of range");
         }
     }
 
-    public void removePiece(int[] position){
+    // 从指定位置移除棋子，将该位置设置为 EmptyPosition
+    public void removePiece(int[] position) {
         int x = position[0];
         int y = position[1];
-        if (x >= 0 && x <= 7 && y >= 0 && y <= 7){
-            board[x][y] = new EmptyPostion();
+        if (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
+            board[x][y] = new EmptyPostion();  // 设置为空位置
             System.out.println("Removed piece from position [" + x + "][" + y + "]");
+        } else {
+            System.out.println("Position out of range");
         }
-        else {
-            System.out.println("out of range");
+    }
 
+    // 打印当前棋盘状态
+    public void boardShow() {
+        for (int i = 0; i <= 7; i = i + 1) {
+            for (int j = 0; j <= 7; j = j + 1) {
+                if (board[i][j] instanceof EmptyPostion) {
+                    System.out.print("[ ]");  // 如果是空棋子，显示空白
+                } else {
+                    System.out.print("[" + board[i][j].nameGetter().charAt(0) + "]");  // 显示棋子的首字母
+                }
+            }
+            System.out.println();
         }
     }
 }
--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args){
-        int[] position = {3,4};
+//        int[] position = {3,4};
 //        SlowPiece slowPiece = new SlowPiece("someone","white",position);
 //        System.out.println(slowPiece);
 
@@ -27,14 +27,30 @@ public class main {
 
 
 
-        FastFlexiblePiece fastFlexiblePiece = new FastFlexiblePiece("fastFlex", "Black", position);
+//        FastFlexiblePiece fastFlexiblePiece = new FastFlexiblePiece("fastFlex", "Black", position);
+//
+//        System.out.println(fastFlexiblePiece);
+//
+//        // 测试向上移动 3 步
+//        fastFlexiblePiece.move("down", 5);
+//        System.out.println("After moving down 3 steps fast:");
+//        System.out.println(fastFlexiblePiece);
+        Board board = new Board();
 
-        System.out.println(fastFlexiblePiece);
+        // 创建棋子
+        int[] position1 = {3, 3};
+        SlowPiece slowPiece = new SlowPiece("boardTest", "White", position1);
 
-        // 测试向上移动 3 步
-        fastFlexiblePiece.move("down", 5);
-        System.out.println("After moving down 3 steps fast:");
-        System.out.println(fastFlexiblePiece);
+        // 将棋子放在棋盘上
+        board.movePiece(slowPiece, position1);
+
+        // 打印棋盘状态
+        board.boardShow();
+
+        // 移除棋子并打印棋盘状态
+        board.removePiece(position1);
+        System.out.println("After removing the piece:");
+        board.boardShow();
     }
 
 

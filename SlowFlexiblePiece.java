90057ff3d2fbad797db258d6cb13fdc6fd5d724d
Niki9001 <w0470221@nscc.ca>
2024-09-21 12:21:21 -0300

    add slow flex piece

---
+++ SlowFlexiblePiece.java
@@ -0,0 +1,6 @@
+public class SlowFlexiblePiece extends Piece{
+    // constructor first
+    public SlowFlexiblePiece (String name, String color, int[] position){
+        super(name, color, position);
+    }
+}

032798c0b26687b3786e5408e242bb87df77b0b1
Niki9001 <w0470221@nscc.ca>
2024-09-21 11:22:40 -0300

    add move to left and right conditions

--- SlowPiece.java
+++ SlowPiece.java
@@ -1,5 +1,15 @@
 public class SlowPiece {
     public void move(String direction){
+        // to adapt both upper and lower case
+        direction = direction.toLowerCase();
+        switch (direction){
+            case "left":
+                break;
+
+            case "right":
+                break;
+
+        }
 
     }
 }

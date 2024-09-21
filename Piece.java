68f37b55250a66da841fea1581fa3c5452489822
Niki9001 <w0470221@nscc.ca>
2024-09-21 10:50:45 -0300

    step 2

--- Piece.java
+++ Piece.java
@@ -4,4 +4,11 @@ public class Piece {
     String color;
     int[] position;
 
+    // Step 2 Piece class
+    public Piece (String name, String color, int[] position){
+        this.name = name;
+        this.color = color;
+        this.position = position;
+    }
+
 }

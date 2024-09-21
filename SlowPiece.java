d9bf99592341a361837fbccdf4049fe2adce0530
Niki9001 <w0470221@nscc.ca>
2024-09-21 11:37:04 -0300

    change move to left

--- SlowPiece.java
+++ SlowPiece.java
@@ -1,11 +1,17 @@
-public class SlowPiece extends Piece{
+public class SlowPiece extends Piece {
     public void move(String direction){
         // to adapt both upper and lower case
         direction = direction.toLowerCase();
         switch (direction){
             case "left":
                 if (positionGetter()[0] > 0){
-
+                    int x = position[0];
+                    x = x - 1;
+                    position[0] = x;
+                    setterPosition(position);
+                }
+                else {
+                    System.out.println("out of range");
                 }
                 break;
 
